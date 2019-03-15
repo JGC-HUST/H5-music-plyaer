079153c42ae4753b3b628ab65ab6c2461850979b
JGC <a1344143728@163.com>
2019-03-15 19:33:44 +0800

    update2

---
+++ data.js
@@ -0,0 +1,27 @@
+/* 数据结构
+{
+    title: '',
+    author: [],
+    src: '',
+    img: '',
+    UUID: 0
+}
+*/
+let data = [
+    {
+        title: 'Shelter',
+        author: [
+            'Madeon',
+            'Porter Robinson'
+        ],//'Madeon/ Porter Robinson'
+        src: 'http://m701.music.126.net/20190315175104/2543fdb55e1f2e4b8a9b98ff01790e9e/jdyyaac/0f08/0009/510f/8bdffaf7817cbc45f0ccfe203ada576f.m4a',
+        img: 'static/shelter.jpg',
+        UUID: 0001
+    },
+    {
+        title: 'In The Hall Of The Mountain King',
+        author: ['Edvard Grieg'],
+        src: 'http://m801.music.126.net/20190315190448/8d89d46e7e561fdebff4526247c79a83/jdyyaac/060c/5159/540c/1fc766b928f3576c4f22cf06b31ea7bd.m4a',
+        img: 'static/hall.jpg'
+    }
+]
---
+++ index.js
@@ -0,0 +1,118 @@
+window.onselectstart = () => false;
+let log = console.log;
+// 定义类的属性
+function player(id) {
+    this.el = document.querySelector(id);
+    this.audio = document.querySelector('audio');
+    this.playList = [];
+    this.playIndex = -1;
+    this.more = this.el.querySelector('.more');
+    this.btns = this.el.querySelector('.btns');
+    this.bgImg = this.el.querySelector('#bg-img');
+    this.songImg = this.el.querySelector('#song-img');
+    this.looped = false;
+}
+// 定义类的方法
+// 下一曲
+player.prototype.next = function () {
+    this.playIndex = (this.playIndex + 1) % this.playList.length;
+    this.audio.src = data[this.playIndex].src;
+    this.renderInfo();
+}
+// 上一曲
+player.prototype.prev = function () {
+    this.playIndex = (this.playIndex - 1 + this.playList.length) % this.playList.length;
+    log(this.playIndex)
+    this.audio.src = data[this.playIndex].src;
+    this.renderInfo();
+}
+player.prototype.toggleLoop = function () {
+    let icon = this.el.querySelector('#loop');
+    if (this.looped) {
+        icon.childNodes[0].src = 'static/menu.png';
+        this.audio.loop = false;
+        this.looped = false;
+    } else {
+        icon.childNodes[0].src = 'static/reload.png';
+        this.audio.loop = true;
+        this.looped = true;
+    }
+}
+player.prototype.like = function () {
+    // todo
+    alert('收藏成功')
+}
+// 初始化函数
+player.prototype.init = function () {
+    let _this = this;
+    this.playList = data;
+    this.playIndex = 0;
+    this.audio.src = data[this.playIndex].src;
+    this.audio.onended = function () {
+        // _this
+        _this.playIndex = (_this.playIndex + 1) % _this.playList.length;
+        _this.audio.src = data[_this.playIndex].src;
+        _this.renderInfo();
+    };
+    this.renderInfo();
+    this.btns.addEventListener('click', function (e) {
+        let target = e.target;
+        let id = target.id || target.parentNode.id;
+        log(id);
+        switch (id) {
+            case 'playTrack':
+                _this.audio.play();
+                _this.changeStatus();
+                break;
+            case 'pauseTrack':
+                _this.audio.pause();
+                _this.changeStatus();
+                break;
+            case 'nextTrack':
+                _this.next();
+                _this.audio.play();
+                _this.changeStatus();
+                break;
+            case 'prevTrack':
+                _this.prev();
+                _this.audio.play();
+                _this.changeStatus();
+                break;
+            case 'loop':
+            log(123)
+                _this.toggleLoop();
+                break;
+            default:
+                break;
+        }
+    })
+}
+// 渲染歌曲信息
+player.prototype.renderInfo = function () {
+    // 更新标题
+    this.el.querySelector('.title').innerText = data[this.playIndex].title;
+    // 更新作者
+    let len = data[this.playIndex].author.length;
+    let author = ''
+    for (let index = 0; index < len - 1; index++) {
+        const element = data[this.playIndex].author[index];
+        author += element + " / "
+    }
+    author += data[this.playIndex].author[len - 1];
+    this.el.querySelector('.author').innerText = author;
+    // 更新图片
+    this.songImg.src = data[this.playIndex].img;
+}
+// 切换播放和暂停的状态
+player.prototype.changeStatus = function () {
+    let playBtn = this.el.querySelector('.play');
+    log(playBtn);
+    if (this.audio.paused) {
+        playBtn.id = 'playTrack';
+        playBtn.childNodes[0].src = 'static/play-circle.png';
+    } else {
+        playBtn.id = 'pauseTrack';
+        playBtn.childNodes[0].src = 'static/time out.png';
+    }
+}
+new player('.wrapper').init();
